9bdeee1802a85bae1b5ba2244f88ac2584c56c14
JasonCacante <jason.cacante@gmail.com>
2024-02-12 21:58:22 -0500

    feat(add-friends-search): friends search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
+  resources :user_stocks, only: %i[create destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search_friend'
 end
